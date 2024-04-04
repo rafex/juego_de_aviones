047c6aea93f97f81a4ee54d7ead0d35056e30dec
rafex <rafex@rafex.dev>
2024-04-04 14:42:20 -0600

    wii

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const port = 3000;
+
 const server = http.createServer((req, res) => {
+  let folderPath = './html';
   console.log(req.url);
   let filePath = '.' + req.url;
   if (filePath === './') {
-    filePath = './index.html';
+    filePath = folderPath + '/index.html';
   }
   const extname = String(path.extname(filePath)).toLowerCase();
   const mimeTypes = {
@@ -32,7 +34,7 @@ const server = http.createServer((req, res) => {
   fs.readFile(filePath, (error, content) => {
     if (error) {
       if (error.code === 'ENOENT') {
-        fs.readFile('./404.html', (error, content) => {
+        fs.readFile(folderPath + '/404.html', (error, content) => {
           res.writeHead(200, { 'Content-Type': 'text/html' });
           res.end(content, 'utf-8');
         });
